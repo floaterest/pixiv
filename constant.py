2c7df3217ff1c8f5d220b78885f5f3e011bffa01
floaterest <56704092+floaterest@users.noreply.github.com>
2021-01-30 16:26:44 -0500

    Add other files

---
+++ constant.py
@@ -0,0 +1,6 @@
+
+class PixivConstant:
+    CLIENT_ID = 'MOBrBDS8blbauoSck0ZfDbtuzpyT'
+    CLIENT_SECRET = 'lsACyCD94FhDUtGTXi3QzcFE2uU1hqtDaKeqrdwj'
+    HASH_SECRET = '28c1fdd170a5204386cb1313c7077b34f83e4aaf4aa829ce78c231e05b0bae2c'
+    HOST = 'https://app-api.pixiv.net'
