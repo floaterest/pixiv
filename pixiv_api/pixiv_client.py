86eb5c8268bec00a240ad1497c88cfc0950ca1d9
floaterest <56704092+floaterest@users.noreply.github.com>
2021-02-03 16:39:02 -0500

    Refactor: split PixivClient into 2 classes

--- pixiv_api/pixiv_client.py
+++ pixiv_api/pixiv_client.py
@@ -1,25 +1,15 @@
-import os
 import json
 import hashlib
-from enum import Enum
 from datetime import datetime
 
 from constant import PixivConstant
 from pixiv_object.token import Token
+from pixiv_api.http_client import HTTPClient
 
-import cloudscraper
-from requests.models import Response
 from urllib3.exceptions import HTTPError
 
 
-class HTTPMethod(Enum):
-    GET = 'GET'
-    POST = 'POST'
-
-
-class PixivClient:
-    client = cloudscraper.create_scraper()
-
+class PixivClient(HTTPClient):
     # region constructor
     def __init__(self, token: Token):
         self.token = token
@@ -32,33 +22,6 @@ class PixivClient:
             'Referer': 'https://app-api.pixiv.net/',
             'Authorization': f'Bearer {token.access_token}'
         }
-
-    # endregion
-
-    # region requests
-    @staticmethod
-    def ensure_sucess_status_code(res: Response) -> bool:
-        """
-        Check status code and raises error if request not successful
-        """
-        if 200 <= res.status_code < 300:
-            return True
-        else:
-            raise HTTPError(res.status_code, res.text)
-
-    @staticmethod
-    def unescape(text: str) -> str:
-        # convert r'\/' or '\\/' to '/'
-        return bytes(text, 'utf8').decode()
-
-    def post(self, url: str, data: dict, object_hook: staticmethod):
-        res = self.client.post(url, data=data)
-        self.ensure_sucess_status_code(res)
-
-        return json.loads(self.unescape(res.text), object_hook=object_hook)
-
-    def get(self, url: str, object_hook: staticmethod = None):
-        pass
 
     # endregion
 
@@ -120,48 +83,4 @@ class PixivClient:
         }
         return PixivClient(PixivClient.get_token(data))
 
-    # endregion
-
-    # region download
-    def download(self, url: str, filename: str = '', override: bool = False, progress: staticmethod = None):
-        """
-        Download an image
-        :param url: url of the image, found in meta_pages or profile_image_urls
-        :param filename: destination filename, will use filename from url if empty
-        :param override: will raise error if false and 'filename' exists
-        :param progress: will be called for each buffer(81920) written
-
-        example of progress:
-            def progress(current:int, total:int):
-                percent = current * 100 // total
-                # current can be greater than total
-                print(min(percent, 100))
-
-        """
-        if not override and os.path.exists(filename):
-            raise FileExistsError
-
-        res = self.client.get(url, stream=True, headers={'Referer': 'https://app-api.pixiv.net/'})
-
-        data = res.raw.data
-        total = len(data)
-        buffer_size = 81920
-        with open(filename, 'wb') as f:
-            # (i + 1) to iterate through every end position of the data
-            # range(... + 1) to include the last part (where the size <= buffer_size) of the data
-            for i in [(i + 1) * buffer_size for i in range(total // buffer_size + 1)]:
-                f.write(data[i - buffer_size:i])
-                if progress:
-                    # i can be greater than total
-                    progress(i, total)
-
-    # endregion
-
-    # region context manager
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.client.close()
     # endregion
