8c9cba0440159219d08cc712ff2139cca3bf10c0
floaterest <56704092+floaterest@users.noreply.github.com>
2021-02-22 16:12:34 -0500

    Refactor user.py

--- pixiv_object/user.py
+++ pixiv_object/user.py
@@ -11,11 +11,15 @@ class User(PixivObject):
     account: str
     profile_image_urls: str
     is_followed: bool
-    comment: str = ''
+    # sometimes there's no comment
+    comment: str = None
 
     # endregion
 
     @staticmethod
     def object_hook(d: dict):
-        d['profile_image_urls'] = d['profile_image_urls']['medium']
+        # if at highest level
+        if 'profile_image_urls' in d:
+            # there's only one url, so no need for an object inside an object
+            d['profile_image_urls'] = d['profile_image_urls']['medium']
         return d
