48925e43cd21d1dfb655dfc2ab10b83b6787821c
floaterest <56704092+floaterest@users.noreply.github.com>
2021-03-12 20:08:18 -0500

    Add implementation of database for User

--- pixiv_object/user.py
+++ pixiv_object/user.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 from pixiv_object.pixiv_object import PixivObject
+from pixiv_database.io import BinaryReader, BinaryWriter
 
 
 @dataclass
@@ -10,8 +11,8 @@ class User(PixivObject):
     name: str
     account: str
     profile_image_urls: str
-    is_followed: bool
-    # sometimes there's no comment
+    # sometimes there's no is_followed or comment (?)
+    is_followed: bool = None
     comment: str = None
 
     # endregion
@@ -23,3 +24,20 @@ class User(PixivObject):
             # there's only one url, so no need for an object inside an object
             d['profile_image_urls'] = d['profile_image_urls']['medium']
         return d
+
+    @staticmethod
+    def read(r: BinaryReader):
+        return User(
+            id=r.read_int(),
+            name=r.read_string(),
+            account=r.read_string(),
+            profile_image_urls=r.read_string(),
+            is_followed=r.read_bool()
+        )
+
+    def write(self, w: BinaryWriter):
+        w.write_int(self.id)
+        w.write_string(self.name)
+        w.write_string(self.account)
+        w.write_string(self.profile_image_urls)
+        w.write_bool(self.is_followed)
