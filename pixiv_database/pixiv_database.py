f578c9215548ac0f52616f4493072776b2fbb48a
floaterest <56704092+floaterest@users.noreply.github.com>
2021-03-14 18:44:22 -0400

    Add database read & write

--- pixiv_database/pixiv_database.py
+++ pixiv_database/pixiv_database.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from pixiv_database.io import BinaryReader, BinaryWriter
 from pixiv_object.illustration import Illustration
 
 
@@ -9,3 +12,21 @@ class PixivDatabase:
     illustrations: list[Illustration]
 
     # endregion
+
+    def __init__(self, path: str):
+        self.path = path
+        with BinaryReader(path) as r:
+            self.last_modified_date = r.read_int()
+            self.username = r.read_string()
+            self.illustrations = [Illustration.read(r) for _ in range(r.read_int())]
+
+    def save_as(self, path: str):
+        with BinaryWriter(path) as w:
+            w.write_int(int(datetime.now().strftime('%Y%m%d')))
+            w.write_string(self.username)
+            w.write_int(len(self.illustrations))
+            for illustration in self.illustrations:
+                illustration.write(w)
+
+    def save(self):
+        self.save_as(self.path)
