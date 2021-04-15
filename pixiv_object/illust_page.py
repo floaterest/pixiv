374f6056924401d73a3860349b2dce9f0be885f1
floaterest <56704092+floaterest@users.noreply.github.com>
2021-04-14 21:04:43 -0400

    Add illust_page dataclass

---
+++ pixiv_object/illust_page.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+from pixiv_object.pixiv_object import PixivObject
+from pixiv_object.pixiv_page import PixivPage
+from pixiv_object.illustration import Illustration
+
+
+@dataclass
+class IllustPage(PixivPage, PixivObject):
+    illusts: list[Illustration]
+    next_url: str
+
+    @staticmethod
+    def object_hook(d: dict):
+        # if at highest level
+        if 'illusts' in d:
+            # convert each dict to Illustration
+            for i, illust in enumerate(d['illusts']):
+                d['illusts'][i] = Illustration(**Illustration.object_hook(illust))
+        return d
