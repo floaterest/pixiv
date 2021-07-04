bba5f893975133410883dde784e812407fabdac9
floaterest <56704092+floaterest@users.noreply.github.com>
2021-07-04 16:37:33 -0400

    change Illustration interface to the same structure as from HTTP get

--- src/types/illustration.ts
+++ src/types/illustration.ts
@@ -1,20 +1,25 @@
 import { Artwork } from './pixiv-object';
 
-export type Illustration = Artwork & {
-	// date of the last successful HTTP request (when visible is true)
-	updated_on: number
+export interface Illustration extends Artwork{
 	type: 'illust' | 'manga' | 'ugoira'
-	// image_urls (included in 'meta_pages')
+	image_urls: {
+		square_medium: string
+		medium: string
+		large: string
+	}
 	tools: string[]
 	width: number
 	height: number
 	sanity_level: number // kinda represents how NSFW the illustration is?
+	series: null // never saw a non-null value
+	meta_single_page: {
+		original_image_url: string
+	}
 	meta_pages: {
 		square_medium: string
 		medium: string
 		large: string
 		original: string
-	}
-	series: null // never saw a non-null value
-	total_comments: number | null // DNE when getting user bookmarks
+	}[]
+	total_comments?: number // DNE when getting user bookmarks
 }
