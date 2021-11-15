d2fe2c25f1412e21fde164c144f41f7e304e7d28
floaterest <56704092+floaterest@users.noreply.github.com>
2021-11-15 15:51:48 -0500

    style(PDB): change read methods to static

--- src/database/pixiv-database.ts
+++ src/database/pixiv-database.ts
@@ -78,74 +78,83 @@ interface Illustration{
 
 export class PixivDatabase{
     //#region properties
-    path: string;
-    // username: string;
-
+    path!: string;
+    username!: string;
     lastModified!: number;
-    illustrations!: [];
-
+    illustrations!: Illustration[];
     //#endregion properties
+
     private r!: BinaryReader;
 
-    constructor(path: string){
-        this.path = path;
-        fs.readFile(path, (err, data) => {
-            if(err) throw err;
+    private constructor(){}
 
-            this.r = new BinaryReader(data);
-        });
-    }
-
-    private read(type: 'Illustration' | 'MetaPage' | 'Tag' | 'User'): Illustration | MetaPage | Tag | User{
+    private static read(
+        r: BinaryReader,
+        type: 'Illustration' | 'MetaPage' | 'Tag' | 'User',
+    ): Illustration | MetaPage | Tag | User{
         switch(type){
             case 'Illustration':
                 return {
-                    id: this.r.int(),
-                    updated_on: this.r.int(),
-                    title: this.r.str(),
-                    type: this.r.byte(),
-                    caption: this.r.str(),
-                    restrict: this.r.byte(),
+                    id: r.int(),
+                    updated_on: r.int(),
+                    title: r.str(),
+                    type: r.byte(),
+                    caption: r.str(),
+                    restrict: r.byte(),
 
-                    user: this.read('User') as User,
-                    tags: Array(this.r.int()).fill(this.read('Tag') as Tag),
-                    tools: Array(this.r.int()).fill(this.r.str()),
-                    create_date: this.r.str(),
-                    width: this.r.int(),
-                    height: this.r.int(),
-                    sanity_level: this.r.byte(),
-                    x_restrict: this.r.bool(),
+                    user: PixivDatabase.read(r, 'User') as User,
+                    tags: Array(r.int()).fill(PixivDatabase.read(r, 'Tag') as Tag),
+                    tools: Array(r.int()).fill(r.str()),
+                    create_date: r.str(),
+                    width: r.int(),
+                    height: r.int(),
+                    sanity_level: r.byte(),
+                    x_restrict: r.bool(),
 
-                    meta_pages: Array(this.r.int()).fill(this.read('MetaPage') as MetaPage),
-                    total_views: this.r.int(),
-                    total_bookmarks: this.r.int(),
-                    is_bookmarked: this.r.bool(),
-                    visible: this.r.bool(),
-                    is_muted: this.r.bool(),
-                    total_comments: this.r.int(),
+                    meta_pages: Array(r.int()).fill(PixivDatabase.read(r, 'MetaPage') as MetaPage),
+                    total_views: r.int(),
+                    total_bookmarks: r.int(),
+                    is_bookmarked: r.bool(),
+                    visible: r.bool(),
+                    is_muted: r.bool(),
+                    total_comments: r.int(),
                 } as Illustration;
             case 'MetaPage':
                 return {
-                    square_medium: this.r.str(),
-                    medium: this.r.str(),
-                    large: this.r.str(),
-                    original: this.r.str(),
+                    square_medium: r.str(),
+                    medium: r.str(),
+                    large: r.str(),
+                    original: r.str(),
                 } as MetaPage;
             case 'Tag':
                 return {
-                    name: this.r.str(),
-                    translated_name: this.r.str(),
+                    name: r.str(),
+                    translated_name: r.str(),
                 } as Tag;
             case 'User':
                 return {
-                    id: this.r.int(),
-                    name: this.r.str(),
-                    account: this.r.str(),
-                    profile_image_urls: this.r.str(),
-                    is_followed: this.r.bool(),
+                    id: r.int(),
+                    name: r.str(),
+                    account: r.str(),
+                    profile_image_urls: r.str(),
+                    is_followed: r.bool(),
                 } as User;
             default:
                 throw Error(`'${type}' is not a recognised type`);
         }
+    }
+
+    static async fromFile(path: string): Promise<PixivDatabase>{
+        let d = new PixivDatabase();
+        d.path = path;
+        await fs.readFile(path, (err, data) => {
+            if(err) throw err;
+
+            let r = new BinaryReader(data);
+            d.lastModified = r.int();
+            d.username = r.str();
+            d.illustrations = Array(r.int()).fill(PixivDatabase.read(r, 'Illustration'));
+        });
+        return d;
     }
 }
