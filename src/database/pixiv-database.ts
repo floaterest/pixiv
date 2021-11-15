b3a2f65fa7e26abea7f5f962f8a46a05915e774c
floaterest <56704092+floaterest@users.noreply.github.com>
2021-11-14 21:01:59 -0500

    style(PD): add comments to interfaces

--- src/database/pixiv-database.ts
+++ src/database/pixiv-database.ts
@@ -13,6 +13,9 @@ interface User{
     id: number
     name: string
     account: string
+    /**
+     * same as profile_image_urls.medium in HTTP response
+     */
     profile_image_urls: string
     is_followed: boolean
 }
@@ -31,20 +34,37 @@ interface MetaPage{
 
 interface Illustration{
     id: number;
+    /**
+     * date of the last successful HTTP request (when visible == True)
+     */
     updated_on: number;
     title: string;
     type: IllustType;
+    // image_urls: included in meta_pages
     caption: string;
     restrict: Restrict;
     user: User;
     tags: Tag[];
     tools: string[];
+    /**
+     * iso
+     */
     create_date: string;
+    // page_count: use meta_pages.length
     width: number;
     height: number;
+    /**
+     * kinda represents how NSFW the illustration is?
+     */
     sanity_level: number;
+    /**
+     * true if r18
+     */
     x_restrict: boolean;
     series: object | null;
+    /**
+     * includes meta_single_page
+     */
     meta_pages: MetaPage[];
     total_views: number;
     total_bookmarks: number;
