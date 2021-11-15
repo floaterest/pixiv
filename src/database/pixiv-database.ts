56e9b3e0a4b0f57fcccf58615fca589462ed3044
floaterest <56704092+floaterest@users.noreply.github.com>
2021-11-15 15:32:00 -0500

    feat(PDB): add read

--- src/database/pixiv-database.ts
+++ src/database/pixiv-database.ts
@@ -95,4 +95,57 @@ export class PixivDatabase{
             this.r = new BinaryReader(data);
         });
     }
+
+    private read(type: 'Illustration' | 'MetaPage' | 'Tag' | 'User'): Illustration | MetaPage | Tag | User{
+        switch(type){
+            case 'Illustration':
+                return {
+                    id: this.r.int(),
+                    updated_on: this.r.int(),
+                    title: this.r.str(),
+                    type: this.r.byte(),
+                    caption: this.r.str(),
+                    restrict: this.r.byte(),
+
+                    user: this.read('User') as User,
+                    tags: Array(this.r.int()).fill(this.read('Tag') as Tag),
+                    tools: Array(this.r.int()).fill(this.r.str()),
+                    create_date: this.r.str(),
+                    width: this.r.int(),
+                    height: this.r.int(),
+                    sanity_level: this.r.byte(),
+                    x_restrict: this.r.bool(),
+
+                    meta_pages: Array(this.r.int()).fill(this.read('MetaPage') as MetaPage),
+                    total_views: this.r.int(),
+                    total_bookmarks: this.r.int(),
+                    is_bookmarked: this.r.bool(),
+                    visible: this.r.bool(),
+                    is_muted: this.r.bool(),
+                    total_comments: this.r.int(),
+                } as Illustration;
+            case 'MetaPage':
+                return {
+                    square_medium: this.r.str(),
+                    medium: this.r.str(),
+                    large: this.r.str(),
+                    original: this.r.str(),
+                } as MetaPage;
+            case 'Tag':
+                return {
+                    name: this.r.str(),
+                    translated_name: this.r.str(),
+                } as Tag;
+            case 'User':
+                return {
+                    id: this.r.int(),
+                    name: this.r.str(),
+                    account: this.r.str(),
+                    profile_image_urls: this.r.str(),
+                    is_followed: this.r.bool(),
+                } as User;
+            default:
+                throw Error(`'${type}' is not a recognised type`);
+        }
+    }
 }
